96ddf7fdaa5d150201034ed84d50ddc5708f2b1e
Valentin C <chatelard.valentin@gmail.com>
2017-09-21 16:11:03 +0200

    Development chart
    Data_dict

--- api/rest.py
+++ api/rest.py
@@ -23,6 +23,24 @@ def test():
 def compute():
     return str(deg_by_min.compute_deg_by_min())
 
+@app.route("/charts_dev")
+def charts_dev():
+    if not 'day' in request.args:
+        day = 7
+    else:
+        day = request.args.get('day')
+    myh_db = db.MyHomessistantDatabase()
+    myh_db.connection()
+    data_dict = myh_db.get_charts_dataset(day)
+
+    # Get act temperature
+    with open(os.path.join(os.environ["MYH_HOME"], "data", "weather.json"), 'r') as weather_file:
+        t_act = json.load(weather_file)["temp_avg"]
+    data_dict["t_act"]="{0:.2f}".format(t_act)
+
+    # Json data_sed ready to be sent to
+    myh_db.close()
+    return render_template("charts_dev.html",**locals())
 
 @app.route("/charts")
 def charts():
