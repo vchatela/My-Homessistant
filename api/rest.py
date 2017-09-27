2e81836c0ff80e94ad94d124365de0e4367fd1e3
Valentin C <chatelard.valentin@gmail.com>
2017-09-27 23:16:59 +0200

    Api : command to turn on/off ready

--- api/rest.py
+++ api/rest.py
@@ -1,27 +1,49 @@
+import json
 import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-import json
 from flask import Flask, render_template, request
-import core.database as db
 
+import core.database as db
 import deg_by_min
+from core.manager import Manager
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Welcome to my world !"
 
 @app.route("/__test")
 def test():
     return "running"
 
 
+@app.route("/command")
+def command():
+    action = request.args.get("action").lower()
+    plug = request.args.get("plug")
+    if not action in ["on", "off"]:
+        return "Error - action must be on or off."
+    with open(os.path.join(os.environ["MYH_HOME"], "data", "plugs.json"), 'r') as plugs_file:
+        plug_data = json.load(plugs_file)
+
+    if not plug in plug_data:
+        return "Error - " + plug + " is not a plug entry."
+    else:
+        plug_data[plug]["plug_state"] = action
+        # save the action for manager
+        with open(os.path.join(os.environ["MYH_HOME"], "data", "plugs.json"), 'w') as plugs_file:
+            json.dump(plug_data, plugs_file)
+        # do the action
+        my_manager = Manager()
+        my_manager.turn_on_off_plug(plug, action)
+
+    return "done"
+
+
 @app.route("/deg_by_min")
 def compute():
     return str(deg_by_min.compute_deg_by_min())
+
 
 @app.route("/charts_dev")
 def charts_dev():
@@ -36,11 +58,12 @@ def charts_dev():
     # Get act temperature
     with open(os.path.join(os.environ["MYH_HOME"], "data", "weather.json"), 'r') as weather_file:
         t_act = json.load(weather_file)["temp_avg"]
-    data_dict["t_act"]="{0:.2f}".format(t_act)
+    data_dict["t_act"] = "{0:.2f}".format(t_act)
 
     # Json data_sed ready to be sent to
     myh_db.close()
-    return render_template("charts_dev.html",**locals())
+    return render_template("charts_dev.html", **locals())
+
 
 @app.route("/dashboard")
 def charts():
@@ -55,11 +78,12 @@ def charts():
     # Get act temperature
     with open(os.path.join(os.environ["MYH_HOME"], "data", "weather.json"), 'r') as weather_file:
         t_act = json.load(weather_file)["temp_avg"]
-    data_dict["t_act"]="{0:.2f}".format(t_act)
+    data_dict["t_act"] = "{0:.2f}".format(t_act)
 
     # Json data_sed ready to be sent to
     myh_db.close()
-    return render_template("dashboard.html",**locals())
+    return render_template("dashboard.html", **locals())
+
 
 if __name__ == '__main__':
     app.logger.setLevel(logging.INFO)  # use the native logger of flask
